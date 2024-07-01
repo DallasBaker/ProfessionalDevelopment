28ae658f0e8efd3dbf782bc3555850470ecd9f20
DallasBaker <118224657+DallasBaker@users.noreply.github.com>
2024-07-01 17:51:20 -0500

    finish Ch1 of Server Driven apps w/ htmx

---
+++ HtmxLearning/CRUD-app/src/server.tsx
@@ -0,0 +1,61 @@
+import {type Context, Hono } from 'hono'
+import {serveStatic} from 'hono/bun'
+
+type Dog = {
+  id: string
+  name: string
+  breed: string
+}
+const dogs = new Map<string, Dog>()
+
+const addDog = (name: string, breed: string): Dog => {
+  const id = crypto.randomUUID()
+  const dog = {id,name,breed}
+  dogs.set(id,dog)
+  return dog
+}
+addDog('Comet', 'Whippet')
+addDog('Oscar', 'German Short-haired Pointer')
+
+const dogRow = (dog: Dog) => {
+  return(
+    <tr class="on-hover">
+      <td>{dog.name}</td>
+      <td>{dog.breed}</td>
+      <td class="buttons">
+        <button class="show-on-hover" hx-delete={`/dog/${dog.id}`} hx-confirm="Are you Sure?" hx-target="closest tr" hx-swap="delete">
+          x
+        </button>
+      </td>
+    </tr>
+  )
+}
+
+const app = new Hono()
+
+app.use('/*', serveStatic({root: './public'}))
+
+app.get('/version', (c: Context) => {
+  return c.text(Bun.version)
+})
+
+app.get('/table-rows', (c:Context) => {
+  const sortedDogs = Array.from(dogs.values()).sort((a, b) => a.name.localeCompare(b.name))
+  return c.html(<>{sortedDogs.map(dogRow)}</>)
+})
+
+app.post('/dog', async (c: Context) => {
+  const formData = await c.req.formData()
+  const name = (formData.get('name') as string) || ''
+  const breed = (formData.get('breed') as string) || ''
+  const dog  = addDog(name, breed)
+  return c.html(dogRow(dog), 201)
+})
+
+app.delete('/dog/:id', (c: Context) => {
+  const id = c.req.param('id')
+  dogs.delete(id)
+  return c.body(null)
+})
+
+export default app
