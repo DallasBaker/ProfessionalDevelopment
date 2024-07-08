5f5bf862a708770908b4ff8b78d8be7dc4cbc826
DallasBaker <dallasjbaker.professional@gmail.com>
2024-07-08 16:50:19 -0500

    Start Ch4

---
+++ HtmxLearning/lazy-loading-demo/src/server.tsx
@@ -0,0 +1,47 @@
+import { type Context, Hono } from 'hono'
+import { serveStatic } from 'hono/bun'
+
+
+const URL = 'https://jsonplaceholder.typicode.com/users';
+const app = new Hono()
+
+type User = {
+  id: number
+  name: string
+  email: string
+  company: {
+    name: string
+  }
+}
+
+
+app.use('/*', serveStatic({ root: './public' }))
+app.get('/users', async (c: Context) => {
+  Bun.sleepSync(1000) // simulates long-running query
+  const res = await fetch(URL)
+  const users = await res.json()
+  return c.html(
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Company</th>
+        </tr>
+      </thead>
+      <tbody>
+        {users.map((user: User) => (
+          <tr>
+            <td>{user.id}</td>
+            <td>{user.name}</td>
+            <td>{user.email}</td>
+            <td>{user.company.name}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
+})
+
+export default app
